7724eab954f8e6441c41aec4c47b3b63eed1fc01
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-20 16:45:02 -0700

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void) {
+/**
+ * This is a "Programing is like building a multiligual puzzle" program in C, written in the Betty style.
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
