2e0c3973783afe9c74d37178105daa4f087c00bc
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-20 17:56:23 -0700

    Tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'simple program in c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*
+	 * this programe use printf to print to console
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
