8e712859e3b3ec7fd0397f0c1ca10306ec478a7d
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-20 17:57:13 -0700

    Tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,6 +12,6 @@ int main(void)
 	/*
 	 * this programe use printf to print to console
 	 */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
