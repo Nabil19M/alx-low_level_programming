86ac222fdc38b8334a313e52296cc6e4c47cb9bd
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-20 17:28:39 -0700

    Tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * This is a "Programing is like building a multiligual puzzle" program in C, written in the Betty style.
+ * main - Entry point
+ *
+ * This is a program in C, written in the Betty style.
+ *
+ * Description :'simple program to print simple sentence using puts'
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
+	/*
+	 * puts() function Prints to the console.
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
