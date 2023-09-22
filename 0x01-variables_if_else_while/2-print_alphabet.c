08dab968be843133533eef8dbc90aea909037473
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-22 23:58:29 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'simple program in c'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int a = 97;
+  int z = 122;
+  
+ for(;a <= z; a++)
+   putchar(a);
+	return (0);
+}
