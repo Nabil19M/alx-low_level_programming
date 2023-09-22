7444ced427c6cc56942813dba24da69729d890ab
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-23 01:29:28 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'simple program in c'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 97;
+	int z = 122;
+
+	for (; z >= a; z--)
+	putchar(z);
+	putchar('\n');
+	return (0);
+}
