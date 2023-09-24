06607b3ccd1aa10d61cdffa30b722796b1c6cbd8
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-25 01:02:45 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'simple program in c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+  return (0);
+}
