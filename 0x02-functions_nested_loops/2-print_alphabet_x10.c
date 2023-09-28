e93973e9d8c1f07324b9a269b9fc27a39314b9d2
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-29 01:11:30 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet - Prints the alphabet in lowercase.
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	int i;
+  int j;
+  
+  for (j = 1; i <= 10; j++)
+    {
+    for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+}
