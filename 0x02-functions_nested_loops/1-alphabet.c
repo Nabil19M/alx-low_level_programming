68805f339fce428c20584d1b6e745c75eb17c29d
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-29 01:00:24 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'simple program in c'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a = 97;
+	int z = 122;
+
+	for (; a <= z; a++)
+	_putchar(a);
+	_putchar('\n');
+	return (0);
+}
