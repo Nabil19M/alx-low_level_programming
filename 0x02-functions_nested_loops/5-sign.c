ce9b827eb81a5ebc39093fb62ba92d928375d68a
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-29 14:05:17 -0700

    Tasks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,30 +2,28 @@
 
 /**
 * print_sign - Returns 1 and prints + if n is greater than zero,
-* Returns 0 and prints 0 if n is zero,
-* Returns -1 and prints - if n is less than zero
 *
-* @c: The character in ASCII code
+* @n: The number in ASCII code
 *
-* Return: 1 for number greater than zero. 
+* Return: 1 for number greater than zero.
 * 0 for number = zero. -1 for n less than zero.
-*
 */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-    _putchar('+');
+		_putchar('+');
 		return (1);
 	}
-    else if (n == 0)
-    {
-      _putchar('0');
-      return (0):
-    }
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
 	else
 	{
-    _putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	_putchar('\n');
