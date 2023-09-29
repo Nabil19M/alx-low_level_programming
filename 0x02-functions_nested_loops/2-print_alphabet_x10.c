ad347b745c480f925e733282a13f303b1b093ddb
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-28 17:21:53 -0700

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
 * print_alphabet_10x - Prints the alphabet in lowercase 10 times.
 *
 * Return: Always 0 (Success)
+*
 */
-
 void print_alphabet_x10(void)
 {
 	int i;
 	int j;
-  
+
 	for (j = 1; i <= 10; j++)
 	{
 		for (i = 97; i <= 122; i++)
