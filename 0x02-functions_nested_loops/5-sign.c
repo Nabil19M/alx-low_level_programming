9f1b340bd0d68fce2c39394c4f84208fd6827558
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-29 23:34:23 +0300

    Create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_sign - Returns 1 and prints + if n is greater than zero,
+* Returns 0 and prints 0 if n is zero,
+* Returns -1 and prints - if n is less than zero
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for number greater than zero. 
+* 0 for number = zero. -1 for n less than zero.
+*
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+    _putchar('+');
+		return (1);
+	}
+    else if (n == 0)
+    {
+      _putchar('0');
+      return (0):
+    }
+	else
+	{
+    _putchar('-');
+		return (-1);
+	}
+	_putchar('\n');
+}
