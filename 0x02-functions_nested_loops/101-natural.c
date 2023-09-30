6f723e79af6b97cbb39de2cc5425f6db8f78061a
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-09-30 16:59:51 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Description: computes and prints the sum of 
+ * all the multiples of 3 or 5 below 1024.
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
   int i;
   int sum = 0;
