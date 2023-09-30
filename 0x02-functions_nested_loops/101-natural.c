0ee4aac97303bc7312cabd1b8405d4f96c88d72e
Nabil19M <nabelmohamed154004@gmail.com>
2023-09-30 07:07:36 -0700

    Tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-  int i;
-  int sum = 0;
+	int i;
+	int sum = 0;
 
-  for (i = 3; i < 1024; i++)
-    {
-      if (i % 3 == 0 || i % 5 == 0)
-      {
-        sum += i;
-      }
-    }
-  putchar('\n');
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	putchar('\n');
 }
