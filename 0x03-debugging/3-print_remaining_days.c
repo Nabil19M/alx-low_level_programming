e521ac0b8c6c099d40e9257bcca3a314c19eb4af
Nabil19M <nabelmohamed154004@gmail.com>
2023-10-02 14:44:36 -0700

    cc

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,8 +30,4 @@ void print_remaining_days(int month, int day, int year)
         }
         else
         {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
-}
+            pr
