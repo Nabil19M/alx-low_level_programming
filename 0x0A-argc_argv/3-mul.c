fa5a351c202b60176d9cb06ad79577f66ca3cdee
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-10-04 20:06:37 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -16,5 +15,4 @@ int main(int argc, char *argv[])
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-	}
 }
