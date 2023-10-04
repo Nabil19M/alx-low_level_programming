dca1c08e77486f226817dd9ad7159147af1107ec
Nabil19M <nabelmohamed154004@gmail.com>
2023-10-04 14:41:03 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*  print_diagonal - prints the chracter \ n times.
+* @n: times the character \ should be printed.
+* Return: no return.
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
