e5a06bbbfbd1f657a0cbf5118101654e91e72e71
Nabil19M <nabelmohamed154004@gmail.com>
2023-10-06 15:52:54 -0700

    tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,10 @@
 void puts2(char *str)
 {
 	int count = 0;
-
-	while (count >= 0)
+	
+	while (count % 2 == 0 && str[count] == '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		_putchar(str[count]);
-		count += 2;
 	}
+	_putchar('\n');
 }
-
