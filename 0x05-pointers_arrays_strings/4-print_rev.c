f24f9c258d11a0233962e4e58065bb25133e77d8
Nabil19M <nabelmohamed154004@gmail.com>
2023-10-06 13:10:44 -0700

    push ya layla

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _print_rev - prints a string reversily, followed by a new line.
+* @s: input string.
+* Return: no return.
+*/
+
+void print_rev(char *s)
+{
+	int size = 0;
+
+	while (*(size + s) != '\0')
+	{
+		size++;
+	}
+	while (size--)
+	{
+		_putchar(s[size]);
+	}
+	_putchar('\n');
+}
