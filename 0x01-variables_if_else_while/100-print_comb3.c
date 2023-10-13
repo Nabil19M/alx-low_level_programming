a9d8d647eab66ce2104d54825b026186cef927f2
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-10-14 01:44:54 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,14 +5,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
-	int arr[] = {01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24, 25, 26, 27, 28, 29, 34, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49, 56, 57, 58, 59, 67, 68, 69, 78, 79, 89};
-	int s;
-
-	for (s = 0; s <= sizeof(arr); s++)
-	putchar(arr[s]);
-	putchar('\n');
-=======
 	int dan;
 	int n;
 
@@ -33,7 +25,5 @@ int main(void)
 		}
 	}
 	putchar(10); /* this is an ascii code for new line*/
-
->>>>>>> 5a33e72659b8dbd8c761bec76609555ff632bf32
 	return (0);
 }
