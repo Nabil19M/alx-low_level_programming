6d56dcfd54da916520ac02dbc3f12939a9af34eb
Nabil19M <115401006+Nabil19M@users.noreply.github.com>
2023-12-13 03:21:37 +0200

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,13 +2,17 @@
 #include "dog.h"
 
 /**
- * free_dog - Frees dogs.
- * @d: The dog to free.
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
