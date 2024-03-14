b2da568d405cab22bfb8cbe7328306a122e12170
Nabil Mohamed <nabelmohamed154004@gmail.com>
2024-03-15 01:54:36 +0200

    include guards

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTES_H
-#define LISTE_H
+#ifndef _LISTE_H_
+#define _LISTE_H_
 #include <stddef.h>
 /**
  * struct list_s - singly linked list
@@ -15,8 +15,6 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
-
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
